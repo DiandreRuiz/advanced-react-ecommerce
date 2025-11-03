086fa6bb22aee2b78d26c4c652363704eedff325
DiandreRuiz <Diandreruiz77@gmail.com>
2025-11-03 14:40:58 -0500

    Changed ui state pattern to use redux instead of component based state

--- src/components/HomePage/CategoriesDropdown.tsx
+++ src/components/HomePage/CategoriesDropdown.tsx
@@ -1,13 +1,18 @@
+import { useState } from "react";
 import Dropdown from "react-bootstrap/Dropdown";
+import DropdownButton from "react-bootstrap/DropdownButton";
 import Spinner from "react-bootstrap/Spinner";
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
 
 const CategoriesDropdown = () => {
+    const [filteredCategories, setFilteredCategories] = useState<string[]>([]);
+
     const fetchCategories = async (): Promise<string[]> => {
         const response = await axios.get("https://fakestoreapi.com/products/categories");
         return response.data;
     };
+
     const { data, isLoading, isError, error } = useQuery<string[]>({
         queryKey: ["categories"],
         queryFn: fetchCategories,
@@ -15,7 +20,15 @@ const CategoriesDropdown = () => {
 
     if (isLoading) return <Spinner />;
     if (isError) return <p color="red">Error getting product categories: {error.message}</p>;
-    return <Dropdown></Dropdown>;
+    return (
+        <DropdownButton id="dropdown-basic-button" title="Category Filter">
+            <Dropdown.Item>
+                {data?.map((categoryName) => (
+                    <Dropdown.Item onClick={}>{categoryName}</Dropdown.Item>
+                ))}
+            </Dropdown.Item>
+        </DropdownButton>
+    );
 };
 
 export default CategoriesDropdown;
