2e154463dae0c62edd43c9790f67d4cebb0cac7f
DiandreRuiz <Diandreruiz77@gmail.com>
2025-11-03 20:12:10 -0500

    Finished building out shoppingCartSlice

--- src/redux/shoppingCartSlice.ts
+++ src/redux/shoppingCartSlice.ts
@@ -2,15 +2,15 @@ import { createSlice, type PayloadAction } from "@reduxjs/toolkit";
 import type { Product } from "../types";
 
 interface ShoppingCartState {
-    items: Product[];
+    products: Product[];
     numberOfItems: number;
     totalPrice: number;
 }
-
+const startingZeroFloat = 0.0;
 const initialState: ShoppingCartState = {
-    items: [],
+    products: [],
     numberOfItems: 0,
-    totalPrice: 0.0,
+    totalPrice: parseFloat(startingZeroFloat.toFixed(2)),
 };
 
 const shoppingCartSlice = createSlice({
@@ -18,10 +18,19 @@ const shoppingCartSlice = createSlice({
     initialState,
     reducers: {
         addItem: (state, action: PayloadAction<Product>) => {
-            const currentItems = state.items;
-            state.items = [...currentItems, action.payload];
+            const currentProducts = state.products;
+            state.products = [...currentProducts, action.payload];
+            state.totalPrice += parseFloat(action.payload.price.toFixed(2));
+            state.numberOfItems += 1;
+        },
+        removeItem: (state, action: PayloadAction<Product>) => {
+            const currentProducts = state.products;
+            state.products = currentProducts.filter((p) => p.id !== action.payload.id);
+            state.totalPrice -= parseFloat(action.payload.price.toFixed(2));
+            state.numberOfItems -= 1;
         },
     },
 });
 
+export const { addItem, removeItem } = shoppingCartSlice.actions;
 export default shoppingCartSlice.reducer;
