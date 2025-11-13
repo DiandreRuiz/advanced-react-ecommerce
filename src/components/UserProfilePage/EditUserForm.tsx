6af36908d0d79ec5142536c03f7f4c37e69b3362
DiandreRuiz <Diandreruiz77@gmail.com>
2025-11-13 12:18:05 -0500

    Finished form initialization fetch from firestore

--- src/components/UserProfilePage/EditUserForm.tsx
+++ src/components/UserProfilePage/EditUserForm.tsx
@@ -7,23 +7,33 @@ import { getDoc, doc } from "firebase/firestore";
 
 const EditUserForm = () => {
     // Firebase auth context
-    const { user } = useFirebaseAuth();
+    const { user, loading, error } = useFirebaseAuth();
 
     const [address, setAddress] = useState<string>("");
     const [name, setName] = useState<string>("");
     const [email, setEmail] = useState<string>("");
     const [password, setPassword] = useState<string>("");
 
+    // Grab the current data from the db for <form> initialization
     useEffect(() => {
         const fetchData = async () => {
             if (!user) throw Error("Unable to find current signed in user!");
             const userDocRef = doc(db, "users", user.uid);
             const userDocSnap = await getDoc(userDocRef);
+
+            // Address not available to edit besides in this field so that
+            // record may not exist in the doc
+            const fetchedAddress: undefined | string = userDocSnap.get("address");
+            setAddress(fetchedAddress ? fetchedAddress : "");
+            setName(userDocSnap.get("name"));
+            setEmail(userDocSnap.get("email"));
+            setPassword(userDocSnap.get("password"));
         };
     });
 
     const handleSubmit = async (e: FormEvent) => {
         e.preventDefault();
+        
     };
 };
 
