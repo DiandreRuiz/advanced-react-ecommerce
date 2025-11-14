0740623d3b8c93ad50587dbeaaf086d9334a0437
DiandreRuiz <Diandreruiz77@gmail.com>
2025-11-14 11:58:59 -0500

    Added interface type for orders

--- src/types.ts
+++ src/types.ts
@@ -8,10 +8,20 @@ export type Product = {
     image: string;
 };
 
+// This is a user profile native to our app (firestore), not Firebase Auth. The IDs match those in Firebase Auth though
+// for easier indexing
 export interface ProfileUser {
     id?: string;
     address?: string; // This is not required to register for a profile, but can be updated after
     name: string;
     email: string;
     password: string;
+}
+
+// Order that was placed by "checking out" with the shopping cart
+export interface Order {
+    id?: string;
+    creationDateTime: Date;
+    total: number;
+    numberOfItems: number;
 }
